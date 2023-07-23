e6df755303183eec2a6399a9c126afb0ef8b049a
ryosuke <kolor0211@gmail.com>
2023-07-23 17:31:04 +0900

    Initial Commit

---
+++ ryo02Kadai2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ryo02Kadai2
+//
+//  Created by 大田涼介 on 2023/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
