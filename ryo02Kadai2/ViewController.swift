7827f10ea7a8a59f9802622863da889844c333a0
ryosuke <kolor0211@gmail.com>
2023-07-23 19:42:29 +0900

    first commit

--- ryo02Kadai2/ViewController.swift
+++ ryo02Kadai2/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var value1: UITextField!
+    @IBOutlet private weak var value2: UITextField!
+    
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    @IBAction private func calculationButton(_ sender: Any) {
+        let inputValue1 = Float(value1.text ?? "") ?? 0
+        let inputValue2 = Float(value2.text ?? "") ?? 0
+        
+        switch  segmentedControl.selectedSegmentIndex{
+        case 0:
+            resultLabel.text = String(inputValue1 + inputValue2)
+        case 1:
+            resultLabel.text = String(inputValue1 - inputValue2)
+        case 2:
+            resultLabel.text = String(inputValue1 * inputValue2)
+        case 3:
+            if (inputValue2 == 0){
+                resultLabel.text = "割る数には0以外を入力してください"
+            } else {
+                resultLabel.text = String(inputValue1 / inputValue2)
+            }
+            resultLabel.text = String(inputValue1 / inputValue2)
+        default:
+            break
+        }
     }
-
-
 }
-
